c182299ce1141c6ed31d60e2847f9834f79ac61c
IMneira <imneira@miuandes.cl>
2025-08-15 13:20:45 -0400

    feat: implement review editing and deletion functionality with user permissions

--- apps/reviews/urls.py
+++ apps/reviews/urls.py
@@ -7,5 +7,7 @@ app_name = "reviews"
 urlpatterns = [
 	path('book/<int:book_id>/create/', views.create_review, name='create'),
 	path('upvote/<int:review_id>/', views.upvote_review, name='upvote'),
+	path('edit/<int:review_id>/', views.edit_review, name='edit'),
+	path('delete/<int:review_id>/', views.delete_review, name='delete'),
 ]
 
